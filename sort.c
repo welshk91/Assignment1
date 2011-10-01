96df5d425ccbf3c22eb123facd2205f03dc481e4
Kevin Welsh <welshk91@gmail.com>
2011-10-01 18:18:17 -0400

    starting sorting program

--- sort.c
+++ sort.c
@@ -11,6 +11,34 @@
 
 int main(){
 
-	printf("***This is a test program***\n");
+	/*Forking*/
+	spawn();
 	return 0;
+}
+
+void  spawn()
+{
+   pid_t  pid;
+   int    status;
+
+   pid = fork();
+
+   if (pid < 0) {		/*Error*/
+            printf("*** ERROR: forking child process failed\n");
+            exit(1);
+   }
+
+  else if (pid == 0) {          /* for the child process:         */
+       printf("***Child code here:   \n");
+	//assign files
+	//sort files
+	//pipe first value to parent
+       printf("***Child code ending\n");
+   }
+
+  else {                                  /* for the parent:      */
+       printf("***Parent Code Here:    \n");
+       while (wait(&status) != pid);       /* wait for completion  */
+       printf("***Parent code ending\n");
+       }
 }
