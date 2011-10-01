e9183e23aedd8235527630e21db40834338c2d17
Kevin Welsh <welshk91@gmail.com>
2011-10-01 18:12:16 -0400

    starting sorting program

---
+++ sort.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <string.h>
+#include <sys/types.h>
+#include <time.h>
+#include <sys/wait.h>
+#include <pthread.h> 
+#include <errno.h> 
+
+int main(){
+
+	printf("***This is a test program***\n");
+	return 0;
+}
